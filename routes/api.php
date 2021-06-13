64af4f2b0d05c1b6177ea22398604e1af17dd0d0
leo dela cruz <leidelosreyes@github.com>
2021-06-13 16:38:21 +0800

    Added APi routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('product')->group(function (){
+    Route::get('/view', [App\Http\Controllers\Api\ProductController::class, 'index'])->name('admin.view');
+    Route::post('/store', [App\Http\Controllers\Api\ProductController::class, 'store'])->name('admin.store');
+    Route::get('/edit/{id}', [App\Http\Controllers\Api\ProductController::class, 'edit'])->name('admin.edit');
+    Route::post('/update/{id}', [App\Http\Controllers\Api\ProductController::class, 'update'])->name('admin.update');
+    Route::delete('/delete/{id}', [App\Http\Controllers\Api\ProductController::class, 'delete'])->name('admin.delete');
+    Route::get('/search', [App\Http\Controllers\Api\ProductController::class, 'search'])->name('admin.search');
+    Route::get('/category',[App\Http\Controllers\Api\ProductController::class, 'getCategory'])->name('admin.category');
+    Route::get('/category/show',[App\Http\Controllers\Api\ProductController::class, 'show_category'])->name('admin.show_category');
+});
